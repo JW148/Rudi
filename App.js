76704fb3d1b37f6a1368165359e19e1617ec42ae
jw148 <wiggin472@gmail.com>
2023-04-15 19:36:55 +0100

    Added (sidebar) navigation functionality So users can go between Till, Stock, and Data

--- App.js
+++ App.js
@@ -1,185 +1,50 @@
 import React, { useCallback, useMemo, useRef, useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Logs } from 'expo'
-import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import { Logs } from 'expo';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 
-import BottomSheet from '@gorhom/bottom-sheet';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import * as FileSystem from 'expo-file-system';
+import Till from './components/Till';
 
-import VegMenu from "./components/VegMenu.js";
-import MeatMenu from './components/MeatMenu.js';
-import CoffeeMenu from './components/CoffeeMennu.js';
-import DrinksMenu from './components/DrinksMenu.js';
-import MiscMenu from './components/MiscMenu.js';
-import { products } from './Data.js';
 
 export default function App() {
   Logs.enableExpoCliLogging();
 
-  const [records, setRecords] = useState([]);
+  // const [records, setRecords] = useState([]);
  
  // This method fetches the records from the database.
- useEffect(() => {
-   async function getRecords() {
-     const response = await fetch(`http://localhost:5000/record/`);
+//  useEffect(() => {
+//    async function getRecords() {
+//      const response = await fetch(`http://localhost:5000/record/`);
  
-     if (!response.ok) {
-       const message = `An error occurred: ${response.statusText}`;
-       window.alert(message);
-       return;
-     }
+//      if (!response.ok) {
+//        const message = `An error occurred: ${response.statusText}`;
+//        window.alert(message);
+//        return;
+//      }
  
-     const records = await response.json();
-     setRecords(records);
-   }
+//      const records = await response.json();
+//      setRecords(records);
+//    }
  
-   getRecords();
+//    getRecords();
  
-   return;
- }, [records.length]);
+//    return;
+//  }, [records.length]);
 
-
-  //state vars
-  const [total, setTotal] = useState(0);
-  const [vegOpen, setVegOpen] = useState(false);
-  const [meatOpen, setMeatOpen] = useState(false);
-  const [coffeeOpen, setCoffeeOpen] = useState(false);
-  const [drinksOpen, setDrinksOpen] = useState(false);
-  const [miscOpen, setMiscOpen] = useState(false);
-  
-  //onPress avent to update the total
-  const handleOnPress = (name) => {
-    let product = products.find(o => o.name === name);
-    setTotal(+(total + product.price).toFixed(3));
-  };
+  const Drawer = createDrawerNavigator();
   
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View style={styles.container}>
-      <View style={styles.row}>
-      <TouchableOpacity style={styles.button} onPress={() => setVegOpen(true)}>
-        <Text>Veg</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => setMeatOpen(true)}>
-        <Text>Meat</Text>
-      </TouchableOpacity>
-      </View>
-      <View style={styles.row}>
-      <TouchableOpacity style={styles.button} onPress={() => setCoffeeOpen(true)}>
-        <Text>Coffee</Text>
-      </TouchableOpacity>
-      </View>
-      <View style={styles.row}>
-      <TouchableOpacity style={styles.button} onPress={() => setDrinksOpen(true)}>
-        <Text>Drinks</Text>
-      </TouchableOpacity>
-      </View>
-      <View style={styles.row}>
-      <TouchableOpacity style={styles.button} onPress={() => handleOnPress('Soup')}>
-        <Text>Soup</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleOnPress('Crisps')}>
-        <Text>Crisps</Text>
-      </TouchableOpacity>
-      </View>
-      <View style={styles.row}>
-      <TouchableOpacity style={styles.button} onPress={() => handleOnPress('Pastry')}>
-        <Text>Pastry</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleOnPress('Treats')}>
-        <Text>Treats</Text>
-      </TouchableOpacity>
-      </View>
-      <View style={styles.row}>
-      <TouchableOpacity style={styles.button} onPress={() => setMiscOpen(true)}>
-        <Text>Misc</Text>
-      </TouchableOpacity>
-      <View style={{flex: 2, justifyContent: 'center', alignItems: 'center'}}>
-        <View style={{flexDirection: 'column'}}>
-        <Text>Total</Text>
-        <Text>£{total}</Text>
-        </View>
-      </View>
-        <TouchableOpacity style={styles.button} onPress={() => setTotal(0)}>
-        <Text>Clear</Text>
-      </TouchableOpacity>
-      </View>
-      {
-        vegOpen && (
-          <VegMenu
-            setVegOpen={setVegOpen}
-            total={total}
-            setTotal={setTotal}
-            products={products}
-          />
-        )
-      }
-      {
-        meatOpen && (
-          <MeatMenu 
-          setMeatOpen={setMeatOpen}
-          total={total}
-          setTotal={setTotal}
-          products={products}
-          />
-        )
-      }
-      {
-        coffeeOpen && (
-          <CoffeeMenu
-            setCoffeeOpen={setCoffeeOpen}
-            total={total}
-            setTotal={setTotal}
-            products={products}
-          />
-        )
-      }
-      {
-        drinksOpen && (
-          <DrinksMenu
-            setDrinksOpen={setDrinksOpen}
-            total={total}
-            setTotal={setTotal}
-            products={products}
-          />
-        )
-      }
-      {
-        miscOpen && (
-          <MiscMenu
-            setMiscOpen={setMiscOpen}
-            total={total}
-            setTotal={setTotal}
-            products={products}
-          />
-        )
-      }
-      <StatusBar 
-      hidden={true} />
-    </View>
-    </GestureHandlerRootView>
+
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Till">
+        <Drawer.Screen name="Till" component={Till} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    paddingHorizontal: 5,
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#DDDDDD',
-    borderColor: 'black',
-    padding: 10,
-    margin: 5,
-    flex: 1,
-    borderRadius: 5,
-  },
-  row: {
-    flex: 1,
-    flexDirection: 'row',
-  }
+  
 });
