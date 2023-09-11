3649afc5c3e0e9c8b72f3fab4fd79f7187d879a5
JW148 <47108195+JW148@users.noreply.github.com>
2023-09-11 18:37:57 +0100

    variable spelling

--- components/sales/DatePicker.js
+++ components/sales/DatePicker.js
@@ -1,86 +1,111 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
-import moment from 'moment';
+import moment from "moment";
 
 const ITEM_WIDTH = 100;
 const ITEM_MARGIN_RIGHT = 10;
 
-export default function DatePicker({day, setDay, monthID, setMonthID}){
-
+export default function DatePicker({ day, setDay, monthID, setMonthID }) {
   //get month names to render in the flatlist
   const months = moment.monthsShort();
-    
-    //gets the layout of the flatlist item to render and index correctly when using initialScrollIndex
-    const getItemLayout = (_, index) => {
-        return {
-          length: ITEM_WIDTH + ITEM_MARGIN_RIGHT,
-          offset: (ITEM_WIDTH + ITEM_MARGIN_RIGHT) * (index - 1),
-          index,
-        };
+
+  //gets the layout of the flatlist item to render and index correctly when using initialScrollIndex
+  const getItemLayout = (_, index) => {
+    return {
+      length: ITEM_WIDTH + ITEM_MARGIN_RIGHT,
+      offset: (ITEM_WIDTH + ITEM_MARGIN_RIGHT) * (index - 1),
+      index,
     };
+  };
 
-    const renderMonths = ({item, index}) => (
-        <TouchableOpacity style={styles.item} onPress={() => setMonthId(index)}>
-          <Text style={index === monthID ? styles.monthSelected : styles.month}>{item}</Text>
-        </TouchableOpacity>
-    );
-
-    const renderDays = ({item, index}) => (
-      <TouchableOpacity style={(index+1) === parseInt(day) ? [styles.item, styles.daySelected] : [styles.item, styles.day]} onPress={() => setDay(index + 1)}>
-        <Text style={[styles.month, {color: (index+1) === parseInt(day) ? 'white' : 'black'}]}>{item + 1}</Text>
-      </TouchableOpacity>
+  const renderMonths = ({ item, index }) => (
+    <TouchableOpacity style={styles.item} onPress={() => setMonthID(index)}>
+      <Text style={index === monthID ? styles.monthSelected : styles.month}>
+        {item}
+      </Text>
+    </TouchableOpacity>
   );
 
-    return(
-        <View style={styles.container}>
-               <FlatList
-                  data={months}
-                  renderItem={renderMonths}
-                  initialScrollIndex={monthID+1}
-                  showsHorizontalScrollIndicator={false}
-                  horizontal={true}
-                  keyExtractor={(item, index) => index}
-                  getItemLayout={getItemLayout}
-               />
-               <FlatList
-                  //get an array of the days in the current month and 
-                  data={[...Array(moment(`2023-${monthID + 1}`, "YYYY-M").daysInMonth()).keys()]}
-                  renderItem={renderDays}
-                  initialScrollIndex={parseInt(day)}
-                  showsHorizontalScrollIndicator={false}
-                  horizontal={true}
-                  keyExtractor={(item, index) => index}
-                  getItemLayout={getItemLayout}
-               />
-        </View> 
-    )
+  const renderDays = ({ item, index }) => (
+    <TouchableOpacity
+      style={
+        index + 1 === parseInt(day)
+          ? [styles.item, styles.daySelected]
+          : [styles.item, styles.day]
+      }
+      onPress={() => setDay(index + 1)}
+    >
+      <Text
+        style={[
+          styles.month,
+          { color: index + 1 === parseInt(day) ? "white" : "black" },
+        ]}
+      >
+        {item + 1}
+      </Text>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={months}
+        renderItem={renderMonths}
+        initialScrollIndex={monthID + 1}
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        keyExtractor={(item, index) => index}
+        getItemLayout={getItemLayout}
+      />
+      <FlatList
+        //get an array of the days in the current month and
+        data={[
+          ...Array(
+            moment(`2023-${monthID + 1}`, "YYYY-M").daysInMonth()
+          ).keys(),
+        ]}
+        renderItem={renderDays}
+        initialScrollIndex={parseInt(day)}
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        keyExtractor={(item, index) => index}
+        getItemLayout={getItemLayout}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    item: {
-        width: ITEM_WIDTH,
-        marginRight: ITEM_MARGIN_RIGHT,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    month: {
-      fontSize: 24,
-      fontWeight: '300'
-    },
-    monthSelected: {
-      fontSize: 32,
-      fontWeight: '400'
-    },
-    day: {
-      borderRadius: 15,
-      backgroundColor: 'lightgrey'
-    },
-    daySelected: {
-      borderRadius: 15,
-      backgroundColor: '#B90016'
-    }
+  container: {
+    flex: 1,
+  },
+  item: {
+    width: ITEM_WIDTH,
+    marginRight: ITEM_MARGIN_RIGHT,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  month: {
+    fontSize: 24,
+    fontWeight: "300",
+  },
+  monthSelected: {
+    fontSize: 32,
+    fontWeight: "400",
+  },
+  day: {
+    borderRadius: 15,
+    backgroundColor: "lightgrey",
+  },
+  daySelected: {
+    borderRadius: 15,
+    backgroundColor: "#B90016",
+  },
 });
